c0f46e0d71412d0fb41f2923264782f6b8ce2e7f
Tuan-gay <gayboyfindfun@gmail.com>
2025-07-01 17:16:27 +0700

    Update and rename ExampleProvider.kt to DvdgayonlineProvider.kt

---
+++ ExampleProvider/src/main/kotlin/com/example/DvdgayonlineProvider.kt
@@ -0,0 +1,21 @@
+package com.Dvdgayonline
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
+import com.lagradost.cloudstream3.extractors.FileMoonSx
+import com.lagradost.cloudstream3.extractors.StreamTape
+
+@CloudstreamPlugin
+class DvdgayonlineProvider: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Dvdgayonline())
+        registerExtractorAPI(EmturbovidExtractor())
+        registerExtractorAPI(FileMoonSx())
+        registerExtractorAPI(DoodPmExtractor())
+        registerExtractorAPI(Vidguardto())
+        registerExtractorAPI(MixDropAG())
+        registerExtractorAPI(Lulustream())
+        registerExtractorAPI(StreamTape())
+    }
